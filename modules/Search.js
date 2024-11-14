c95772d30525e4641738edae49a630122a649b11
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-11-14 17:23:20 +0100

    first commit

---
+++ modules/Search.js
@@ -0,0 +1,57 @@
+import { Wind } from '../modules/Wind.js'
+
+class Search {
+   constructor() {
+      // definir les variables
+      this.input = document.querySelector(".js-search-input");
+      this.form = document.querySelector(".js-search-form");
+      this.cities = [];
+      // lancer les methodes
+      this.init();
+   }
+
+   init() {
+      this.getCities();
+      this.watchUserInput();
+   }
+
+   watchUserInput() {
+      this.form.addEventListener("submit", (event) => {
+         event.preventDefault();
+         this.getLagLong();
+      });
+   }
+
+   getLagLong() {
+      const name = this.input.value;
+      const cityData = this.getCityData(name);
+      if(cityData) {
+        const lat = cityData.lat
+        const long = cityData.lng
+        console.log(lat,long)
+        new Wind({lat, long})
+      } else {
+        alert('La ville renseignée n\'existe pas!')
+      }
+
+   }
+
+   getCities() {
+      fetch("../data/french-cities.json")
+         .then((response) => response.json())
+         .then((data) => {
+            this.cities = data;
+         });
+   }
+
+   getCityData(cityName) {
+      const cityNameLower = cityName.toLowerCase();
+      const cityData = this.cities.find(
+         (cityObject) =>
+            cityObject.city.toLowerCase() === cityNameLower
+      );
+      return cityData;
+   }
+}
+
+export { Search };
---
+++ modules/Wind.js
@@ -0,0 +1,34 @@
+class Wind {
+    constructor(props){
+        const {lat, long} = props
+        this.lat = lat
+        this.long = long
+        this.url = ''
+        this.init()
+    }
+
+    init(){
+        this.buildUrl()
+        this.getWindData()
+    }
+
+    buildUrl(){
+        const base = 'https://api.open-meteo.com/v1/forecast'
+        const requiredLatitude = 'latitude=' + this.lat
+        const requiredLongitude = 'longitude=' + this.long
+
+        const params = ['wind_speed_10m','wind_direction_10m']
+        const paramsStringList = params.join(',')
+
+        this.url = `${base}?${requiredLatitude}&${requiredLongitude}&current=${paramsStringList}`
+    }
+    getWindData(){
+        fetch(this.url)
+        .then(response => response.json())
+        .then((data)=> {
+            console.log(data, 'data')
+        })
+    }
+}
+
+export { Wind }
