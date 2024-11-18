2f3961e2e0f6d9617eec7fbcd7cb681e0c4e0b7e
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-11-18 14:16:08 +0100

    feat(three): add flower

--- fleur/modules/Flower.js
+++ fleur/modules/Flower.js
@@ -38,6 +38,8 @@ class Flower {
          1000
       );
       this.camera.position.z = 100;
+      this.camera.position.x = 0;
+      this.camera.position.y = 0;
    }
 
    createOrbitControls() {
@@ -54,16 +56,51 @@ class Flower {
    }
 
    createStem() {
-      const geometry = new THREE.CylinderGeometry(2, 2, 20, 32);
+      this.stemHeight = 50;
+      const geometry = new THREE.CylinderGeometry(
+         2,
+         2,
+         this.stemHeight,
+         32
+      );
       const color = new THREE.Color("rgb(0, 255, 0)");
       const material = new THREE.MeshBasicMaterial({ color: color });
       const cylinder = new THREE.Mesh(geometry, material);
       this.scene.add(cylinder);
    }
 
-   createPistil() {}
+   createPistil() {
+      const geometry = new THREE.SphereGeometry(3, 32, 16);
+      const material = new THREE.MeshBasicMaterial({
+         color: 0xffff00,
+      });
+      const sphere = new THREE.Mesh(geometry, material);
+      sphere.position.y = this.stemHeight / 2;
+      this.scene.add(sphere);
+   }
 
-   createPetals() {}
+   createPetals() {
+      const geometry = new THREE.TorusGeometry(6, 1, 16, 100);
+      const color = new THREE.Color(255, 0, 0);
+      const material = new THREE.MeshBasicMaterial({
+         color: color,
+      });
+      const positionsX = [-5, 0, 5, 0]
+      const positionsZ = [0, -5, 0, 5]
+      const rotationsY = [-30, -5, 30, 0]
+      const rotationsX = [90, 120, 90, 60]
+      for (let i = 0; i < 4; i++) {
+const torus = new THREE.Mesh(geometry, material);
+      torus.position.y = this.stemHeight / 2;
+      torus.rotation.x = THREE.MathUtils.degToRad(rotationsX[i]);
+      torus.position.x = positionsX[i];
+      torus.position.z = positionsZ[i];
+      torus.rotation.y = THREE.MathUtils.degToRad(rotationsY[i]);
+      this.scene.add(torus);
+      }
+
+      
+   }
 
    animate() {
       requestAnimationFrame(this.animate.bind(this));
